cf8591dccd3f0eb83600203feafcd5803020b266
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    msm8610-common: clean up cmdline
    * console is overwritten by bootloader
    * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
    * same deal for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I816a457f173acce54c12383a5513441563f9e810

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x37 vmalloc=400M
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M
 
 WLAN_MODULES:
 	mkdir -p $(KERNEL_MODULES_OUT)/pronto
