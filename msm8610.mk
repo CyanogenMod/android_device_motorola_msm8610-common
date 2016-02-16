3802a57f557194ef7937895837f689ec8c5b01a1
Ashwin Ramesh <ashwinr64@gmail.com>
2016-02-16 04:25:01 -0800

    msm8610-common: Commonize Audio configuration [3/3]
    Change-Id: Ieec0c218111b913826aa4dbd1f1ef26a8ed586ad

--- msm8610.mk
+++ msm8610.mk
@@ -66,6 +66,12 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay
 
+# Audio Configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
