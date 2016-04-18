6fb08aa9a0cd7357a2423d4d6418a10fd44175e0
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-04-18 00:28:17 -0400

    msm8610-common: cmhw: fix KCAL path
    Change-Id: I5a9ad792c6d3c13e7bacab725fd916f1d284834e

--- cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
+++ cmhw/org/cyanogenmod/hardware/DisplayColorCalibration.java
@@ -22,7 +22,7 @@ import java.io.File;
 
 public class DisplayColorCalibration {
 
-    private static final String KCAL_TUNING_FILE = "/sys/devices/virtual/graphics/fb0/kcal_enable";
+    private static final String KCAL_TUNING_FILE = "/sys/devices/virtual/graphics/fb0/kcal";
 
     public static boolean isSupported() {
         File file = new File(KCAL_TUNING_FILE);
