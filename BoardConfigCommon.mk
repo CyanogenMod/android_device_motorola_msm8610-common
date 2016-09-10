eb7687d0794e40220085b0cfab8df82a9a6043bf
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-09-09 21:38:40 -0400

    msm8610-common: N temp
    Change-Id: I08217291b354a6ed508743c7d0ba270e2dae66eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androidkernel-
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 vmalloc=400M androidboot.selinux=permissive
 
 TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
